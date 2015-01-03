ca3c463b8b31f25e004436a70fd608cc4c635ead
Qiki <qi@brandingbrand.com>
2015-01-03 17:10:11 -0500

    Initial Commit

---
+++ IBOutletCollectionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IBOutletCollectionDemo
+//
+//  Created by kiki on 1/3/15.
+//  Copyright (c) 2015 kiki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IBOutletCollectionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IBOutletCollectionDemo
+//
+//  Created by kiki on 1/3/15.
+//  Copyright (c) 2015 kiki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
